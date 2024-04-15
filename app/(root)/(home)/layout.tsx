e359c691d9adcc846723594a6e896fd837e52345
Denitsa Slavkova <deni9309@gmail.com>
2024-04-15 19:44:26 +0300

    Add static files; Add routes for (home)

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,13 +1,16 @@
 import React, { ReactNode } from 'react';
 
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 const HomeLayout = ({ children }: { children: ReactNode; }) => {
   return (
     <main className="relative">
-      Navbar
+      <Navbar />
 
       <div className="flex">
-        Sidebar
-        
+        <Sidebar />
+
         <section className="flex flex-col flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">
             {children}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,37 @@
+'use client';
+import React from 'react';
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="global-sidebar">
+      <div className="flex flex-col flex-1 gap-6">
+        {sidebarLinks.map(link => {
+          const isActive = link.route === pathname || pathname.startsWith(link.route);
+
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={cn('flex items-center justify-start gap-4 p-4 rounded-lg', {
+                'bg-blue-1': isActive,
+              })}
+            >
+              <Image src={link.imgUrl} alt={link.label} width={24} height={24} />
+              <p className="font-semibold max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
