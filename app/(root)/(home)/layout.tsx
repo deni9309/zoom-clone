2d7001a313e5d7ff75a516e7d7637ec03d6209d9
Denitsa Slavkova <deni9309@gmail.com>
2024-04-15 19:00:48 +0300

    Configure project layouts

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+
+const HomeLayout = ({ children }: { children: ReactNode; }) => {
+  return (
+    <main className="relative">
+      Navbar
+
+      <div className="flex">
+        Sidebar
+        
+        <section className="flex flex-col flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">
+            {children}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default HomeLayout;
