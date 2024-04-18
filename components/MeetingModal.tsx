3500de8f1734fb77cc030761fc2bf7ef365b732d
Denitsa Slavkova <deni9309@gmail.com>
2024-04-18 19:06:12 +0300

    Create MeetingModal reusable component

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Image from "next/image";
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 type MeetingModalProps = {
   isOpen: boolean;
@@ -23,9 +28,29 @@ const MeetingModal = ({
   image,
   handleClick
 }: MeetingModalProps) => {
- 
+
   return (
-    <div>MeetingModal</div>
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex flex-col gap-6 w-full max-w-[520px] border-none bg-dark-1 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="image" width={72} height={72} />
+            </div>
+          )}
+          <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>{title}</h1>
+          
+          {children}
+          
+          <Button onClick={handleClick} className="bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0">
+            {buttonIcon && (
+              <Image src={buttonIcon} alt="button icon" width={13} height={13} />
+            )} &nbsp;
+            {buttonText || 'Schedule Meeting'}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
